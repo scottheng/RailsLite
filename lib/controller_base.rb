14cb64479c95f33d688845f8c65689be857deb21
Scott Heng <scottheng3@gmail.com>
2017-02-21 15:49:00 -0800

    Add session to ControllerBase

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -27,6 +27,7 @@ class ControllerBase
     res.status = 302
     res['Location'] = url
     @already_built_response = true
+    session.store_session(res)
 
   end
 
@@ -38,7 +39,7 @@ class ControllerBase
     res['Content-Type'] = content_type
     res.write(content)
     @already_built_response = true
-
+    session.store_session(res)
   end
 
   # use ERB and binding to evaluate templates
@@ -57,6 +58,7 @@ class ControllerBase
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
--- lib/session.rb
+++ lib/session.rb
@@ -4,16 +4,28 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    cookie = req.cookies['_rails_lite_app']
+    @cookie_key_val = cookie.nil? ? {} : JSON.parse(cookie)
+
   end
 
   def [](key)
+    @cookie_key_val.each do |name, val|
+      return val if key == name
+    end
+    nil
   end
 
   def []=(key, val)
+    @cookie_key_val[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.set_cookie('_rails_lite_app', {
+    path: '/',
+    value: @cookie_key_val.to_json
+  })
   end
 end
