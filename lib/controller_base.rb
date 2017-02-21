4cf85f02139612dded1edde9cfc6f3ca49c5ac2d
Scott Heng <scottheng3@gmail.com>
2017-02-21 15:01:27 -0800

    Create ControllerBase, add methods for render_content and render

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -2,31 +2,57 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
+require 'byebug'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
+
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
+
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "Double Render error" if already_built_response?
+    res.status = 302
+    res['Location'] = url
+    @already_built_response = true
+
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "Double Render error" if already_built_response?
+    res['Content-Type'] = content_type
+    res.write(content)
+    @already_built_response = true
+
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    raise "Double Render error" if already_built_response?
+    controller_name = self.class.name.underscore()
+    path = "views/#{controller_name}/#{template_name}.html.erb"
+    content = File.read(path)
+    template = ERB.new(content)
+    eval_template = template.result(binding)
+
+    render_content(eval_template, "text/html")
+    @already_built_response = true
   end
 
   # method exposing a `Session` object
@@ -37,4 +63,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
