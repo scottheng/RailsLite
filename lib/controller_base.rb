b9c246141b5bee35431fbdce911597549d4ec26b
Scott Heng <scottheng3@gmail.com>
2017-02-21 17:35:42 -0800

    Create Route class

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -6,10 +6,10 @@ require 'byebug'
 
 class ControllerBase
   attr_reader :req, :res, :params
-
+  include Singleton
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, params = {})
     @req = req
     @res = res
     @already_built_response = false
@@ -63,5 +63,7 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    self.send(:render, template_name) unless already_built_response
   end
 end
